579a0c9e7314ad3453c443773060ba787323626c
Zehan Khan <zkhan@hootboard.com>
2023-05-13 03:56:31 +0530

    style: :art: setup and test tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import { Fragment, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import TailwindComponentForTest from "./TailwindComponentForTest";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <Fragment>
-      <h1>App</h1>
+      <TailwindComponentForTest />
     </Fragment>
   );
 }
