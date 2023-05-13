8adc3d30e29874dafa4c23e5730e994e8036a438
Zehan Khan <zkhan@hootboard.com>
2023-05-13 14:31:30 +0530

    feat: :sparkles: set abortController on fetch in useEffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import "./App.css";
-import Login from "./page/Login";
+import Login from "./page/screen/Login";
 
 function App() {
+  
+  useEffect(() => {
+    console.log(import.meta);
+    let abortController = new AbortController();
+    const fetchData = async () => {
+      try {
+        const res = await fetch(endpoint, {
+          ...options,
+          signal: abortController.signal,
+        });
+        const data = await res.json();
+      } catch (error) {
+        console.log(error.message);
+        if (error.name === "AbortError") {
+          console.log(error.name);
+        }
+      }
+    };
+    return () => abortController.abort();
+  }, []);
 
   return (
     <Fragment>
