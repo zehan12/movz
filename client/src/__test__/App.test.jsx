e57e16d4708bc8fc0162feaff165293eeef4cfee
Zehan Khan <zkhan@hootboard.com>
2023-05-24 01:05:13 +0530

    test: :test_tube: wrote test cases for context

--- client/src/__test__/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-describe('App',()=>{
-    it('should have healine h1 Tailwind install',()=>{
-        render(<App />);
-        expect(screen.getByText(/App/i)).toBeInTheDocument();
-    })
-})
